24c909abead0bf15fbac13c789a5f503a96e90de
Noé Pozza <noe.pozza@student.unamur.be>
2023-10-24 22:13:56 +0200

    add popup, restructurate repo

--- main.js
+++
@@ -1,8 +0,0 @@
-import { glass_effect, dark_mode } from "./Config";
-
-import { modifyStyle } from "./modifyStyle";
-
-
-// Here call the function following user config
-
-modifyStyle(dark_mode, glass_effect);
